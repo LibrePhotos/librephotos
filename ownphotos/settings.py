4c0656297fdb52c5504c44e0f940e97a1635a68b
Hooram Nam <hooram@podri.org>
2017-07-17 07:27:45 +0900

    try mysql backend

--- ownphotos/settings.py
+++ ownphotos/settings.py
@@ -86,25 +86,25 @@ WSGI_APPLICATION = 'ownphotos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'ownphotos',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': '172.17.0.2',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'ownphotos',
+#         'USER': 'root',
+#         'PASSWORD': 'password',
+#         'HOST': '172.17.0.2',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
 
 
 
