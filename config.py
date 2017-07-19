ae6b42452250fdeff00e6cfb7456924fe04982e2
Hooram Nam <hooram@podri.org>
2017-07-19 20:41:39 +0900

    Close face images when training. Fixed too many open files error when training the face classifier a bunch of times.

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 # it will not look for photos recursively, so you might want to add subdirectories as well.
 
 image_dirs = [
-	'/home/rammi/Downloads/tuebingen',
-	# '/Users/hooram/ownCloud/Photos/korea',
+	# '/home/rammi/Downloads/tuebingen',
+	# '/Users/hooram/Nextcloud/Photos/korea',
 	# '/Users/hooram/Downloads',
-	# '/Users/hooram/ownCloud/Camera Uploads',
+	'/Users/hooram/Nextcloud/Camera Uploads',
 ]
