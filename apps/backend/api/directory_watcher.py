5ecc903e295d46c2cae17fc438f00de9e155c702
Hooram Nam <hooram@podri.org>
2017-08-02 01:24:14 +0900

    django backend communicates with densecap. add captions field to photo model. disable geolocating each photo for now (I think I went over the Nominatim quota)

--- apps/backend/api/directory_watcher.py
+++ apps/backend/api/directory_watcher.py
@@ -4,9 +4,10 @@ import os
 import datetime
 from tqdm import tqdm
 import hashlib 
-
+import pytz
 from config import image_dirs
 
+import ipdb
 
 def is_photos_being_added():
     photo_count = Photo.objects.count()
@@ -45,7 +46,8 @@ def scan_photos():
 
                 if qs.count() < 1:
                     photo = Photo(image_path=img_abs_path)
-                    photo.added_on = datetime.datetime.now()
+                    photo.added_on = datetime.datetime.now().replace(tzinfo=pytz.utc)
+                    photo.geolocation_json = {}
                     photo.save()
                     photo._generate_md5()
                     
@@ -54,78 +56,12 @@ def scan_photos():
                     elapsed = (datetime.datetime.now() - start).total_seconds()
                     print('thumbnail get', elapsed)
 
-                    start = datetime.datetime.now()
-                    photo._save_image_to_db()
-                    elapsed = (datetime.datetime.now() - start).total_seconds()
-                    print('image save', elapsed)
 
                     start = datetime.datetime.now()
-                    photo._extract_exif()
-                    photo.save()
+                    photo._generate_captions()
                     elapsed = (datetime.datetime.now() - start).total_seconds()
-                    print('exif extraction', elapsed)
+                    print('caption generation', elapsed)
 
-                    start = datetime.datetime.now()
-                    photo._geolocate()
-                    photo.save()
-                    elapsed = (datetime.datetime.now() - start).total_seconds()
-                    print('geolocation', elapsed)
-
-                    start = datetime.datetime.now()
-                    photo._extract_faces()
-                    elapsed = (datetime.datetime.now() - start).total_seconds()
-                    print('face extraction', elapsed)
-
-                    start = datetime.datetime.now()
-                    photo._add_to_album_date()
-                    elapsed = (datetime.datetime.now() - start).total_seconds()
-                    added_photo_count += 1
-                    print(img_abs_path)
-                else:
-                    print("photo already exists in db")
-            except Exception as e:
-                print("could not load image %s"%image_path)
-                try:
-                    print(e.message)
-                except:
-                    pass
-
-    return {"new_photo_count":added_photo_count, "status":True}
-    # photos = Photo.objects.all()
-    # for photo in photos:
-    #     print(photo.image_hash)
-    #     faces = photo.face_set.all()
-    #     print(photo.face_set.all())
-
-
-if __name__ == "__main__":
-    image_paths = []
-    for image_dir in image_dirs:
-        image_paths.extend([os.path.join(dp, f) for dp, dn, fn in os.walk(image_dir) for f in fn]
-    )
-
-    added_photo_count = 0
-    for image_path in tqdm(image_paths):
-        if image_path.lower().endswith('.jpg'):
-            try:
-                img_abs_path = image_path
-                hash_md5 = hashlib.md5()
-                with open(img_abs_path, "rb") as f:
-                    for chunk in iter(lambda: f.read(4096), b""):
-                        hash_md5.update(chunk)
-                image_hash = hash_md5.hexdigest()
-                qs = Photo.objects.filter(image_hash=image_hash)
-
-                if qs.count() < 1:
-                    photo = Photo(image_path=img_abs_path)
-                    photo.added_on = datetime.datetime.now()
-                    photo.save()
-                    photo._generate_md5()
-                    
-                    start = datetime.datetime.now()
-                    photo._generate_thumbnail()
-                    elapsed = (datetime.datetime.now() - start).total_seconds()
-                    print('thumbnail get', elapsed)
 
                     start = datetime.datetime.now()
                     photo._save_image_to_db()
@@ -162,3 +98,12 @@ if __name__ == "__main__":
                     print(e.message)
                 except:
                     pass
+
+    return {"new_photo_count":added_photo_count, "status":True}
+    # photos = Photo.objects.all()
+    # for photo in photos:
+    #     print(photo.image_hash)
+    #     faces = photo.face_set.all()
+    #     print(photo.face_set.all())
+
+
--- apps/backend/ownphotos/settings.py
+++ apps/backend/ownphotos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'api',
     'rest_framework',
     'corsheaders',
