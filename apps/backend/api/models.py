245fc237bcd2fa63dbcbca3fb7da0e037d8132e6
user <user@hub.Home>
2017-10-20 22:45:44 +0200

    Added transpose for orientation correction for display

--- apps/backend/api/models.py
+++ apps/backend/api/models.py
@@ -57,6 +57,7 @@ class Photo(models.Model):
     exif_gps_lat = models.FloatField(blank=True, null=True)
     exif_gps_lon = models.FloatField(blank=True, null=True)
     exif_timestamp = models.DateTimeField(blank=True,null=True,db_index=True)
+	exif_orientation = models.TextField(blank=true, null=True,db_index=False)
 
     exif_json = JSONField(blank=True,null=True)
 
@@ -87,12 +88,39 @@ class Photo(models.Model):
 
     def _generate_thumbnail(self):
         image = PIL.Image.open(self.image_path)
+		
+		#If no ExifTags, no rotating needed.
+		try:
+			# Grab orientation value.
+			image_exif = image._getexif()
+			image_orientation = image_exif[274]
+
+			# Rotate depending on orientation.
+			if image_orientation == 2:
+				image = image.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+			if image_orientation == 3:
+				image = image.transpose(PIL.Image.ROTATE_180)
+			if image_orientation == 4:
+				image = image.transpose(PIL.Image.FLIP_TOP_BOTTOM)
+			if image_orientation == 5:
+				image = image.transpose(PIL.Image.FLIP_LEFT_RIGHT).transpose(PIL.Image.ROTATE_90)
+			if image_orientation == 6:
+				image = image.transpose(PIL.Image.ROTATE_270)
+			if image_orientation == 7:
+				image = image.transpose(PIL.Image.FLIP_TOP_BOTTOM).transpose(PIL.Image.ROTATE_90)
+			if image_orientation == 8:
+				image = image.transpose(PIL.Image.ROTATE_90)
+		
+		except:
+			pass
+				
         # make aspect ration preserved thumbnail
         image.thumbnail(ownphotos.settings.THUMBNAIL_SIZE, PIL.Image.ANTIALIAS)
         image_io_thumb = BytesIO()
         image.save(image_io_thumb,format="JPEG")
         self.thumbnail.save(self.image_hash+'.jpg', ContentFile(image_io_thumb.getvalue()))
         image_io_thumb.close()
+		
         # make square thumbnail
         square_thumb = ImageOps.fit(image, ownphotos.settings.THUMBNAIL_SIZE, PIL.Image.ANTIALIAS)
         image_io_square_thumb = BytesIO()
@@ -132,9 +160,14 @@ class Photo(models.Model):
                     tst_dt = datetime.strptime(tst_str,"%Y-%m-%d %H:%M:%S")                     
                 # ipdb.set_trace()
                 self.exif_timestamp = tst_dt
-
             else:
                 self.exif_timestamp = None
+				
+			if 'EXIF Orientation' in exif.keys():
+                ornt_str = exif['EXIF Orientation'].values
+                self.exif_orientation = ornt_str
+            else:
+                self.exif_orientation = None
 
             if 'GPS GPSLongitude' in exif.keys():
                 self.exif_gps_lon = util.convert_to_degrees(exif['GPS GPSLongitude'].values)
