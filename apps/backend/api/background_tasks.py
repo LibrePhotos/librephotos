2261579a9a802db459926173ebad69e9301481d4
Hooram Nam <hooram@podri.org>
2018-04-16 19:03:24 +0900

    include names of people in photo serializer, add all captions instead of first 10 from densecap when generating captions, add option to generate captions for photos with existing captions as a background task

--- apps/backend/api/background_tasks.py
+++ apps/backend/api/background_tasks.py
@@ -1,10 +1,14 @@
 from api.models import Photo
 from api.util import logger
 
-def generate_captions():
-    photos = Photo.objects.filter(search_captions=None)
+def generate_captions(overwrite=False):
+    if overwrite:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(search_captions=None)
     logger.info('%d photos to be processed for caption generation'%photos.count())
     for photo in photos:
+        logger.info('generating captions for %s'%photo.image_path)
         photo._generate_captions()
         photo.save()
 
@@ -15,4 +19,5 @@ def geolocate(overwrite=False):
         photos = Photo.objects.filter(geolocation_json=None)
     logger.info('%d photos to be geolocated'%photos.count())
     for photo in photos:
+        logger.info('geolocating %s'%photo.image_path)
         photo._geolocate_mapbox()
