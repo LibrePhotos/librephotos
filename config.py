2c6c1ad774b2366ab30d148fc6c6591ff044f11b
Hooram Nam <hooram@podri.org>
2018-06-11 00:54:47 +0900

    update readme

--- config.py
+++ config.py
@@ -3,8 +3,23 @@
 import os
 
 image_dirs = [
-	'/home/rammi/Downloads/tuebingen',
+    # '/home/hooram/with_exif/'
+    '/home/hooram/Nextcloud/InstantUpload',
+    '/home/hooram/Nextcloud/Camera Uploads',
+    # '/home/hooram/Nextcloud/Photos/May 26th',
+    # '/home/hooram/Nextcloud/kakaotalk',
+    # '/mnt/ext/facebook_hooram/photos',
+    # '/mnt/ext/pictures/DCIM',
+    # '/mnt/ext/pictures/unorganized',
+    # '/mnt/ext/pictures/Pictures',
+    # '/mnt/ext/pictures/Android Photo Backup',
+    # '/mnt/ext/pictures/Camera Uploads',
+    # '/mnt/ext/pictures/hiking with anton and andrea',
+    # '/mnt/ext/pictures/Aperture Library.aplibrary/Masters/',
+    # '/mnt/ext/pictures/Aperture Library.aplibrary/Masters',
+    # '/'
+
 ]
 
-mapzen_api_key = os.environ['MAPZEN_API_KEY']
+mapzen_api_key = 'take_care_of_me'
 mapbox_api_key = os.environ['MAPBOX_API_KEY']
