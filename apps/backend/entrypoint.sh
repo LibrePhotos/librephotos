2a2234f56654c37d56a8f79de4861dbd2a1354a3
Hooram Nam <hooram@podri.org>
2018-06-11 10:17:25 +0900

    include frontend in docker

--- apps/backend/entrypoint.sh
+++ apps/backend/entrypoint.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
 cp /code/nginx.conf /etc/nginx/sites-enabled/default
-
 sed -i -e 's/replaceme/'"$BACKEND_HOST"'/g' /etc/nginx/sites-enabled/default
-
 service nginx restart
 
 source /venv/bin/activate
@@ -17,8 +15,14 @@ User.objects.filter(email='$ADMIN_EMAIL').delete()
 User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')
 EOF
 
-echo "Running server..."
+echo "Running backend server..."
 
 python manage.py rqworker default &
-gunicorn --bind 0.0.0.0:8001 ownphotos.wsgi
+gunicorn --bind 0.0.0.0:8001 ownphotos.wsgi &
 
+
+
+sed -i -e 's/replaceme/'"$BACKEND_HOST"'/g' /code/ownphotos-frontend/src/api_client/apiClient.js
+cd /code/ownphotos-frontend
+npm run build
+serve -s build
