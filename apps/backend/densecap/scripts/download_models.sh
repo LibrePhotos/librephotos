e3385b5d7b3ad4045d0225f54201c8191d6f51db
Niaz Faridani-Rad <faridani.rad@gmail.com>
2021-01-10 16:59:23 +0100

    [REF] Delete densecap

--- apps/backend/densecap/scripts/download_models.sh
+++
@@ -1,6 +0,0 @@
-# Download the VGG-16 model
-mkdir -p data/models/vgg-16
-cd data/models/vgg-16
-wget https://gist.github.com/ksimonyan/211839e770f7b538e2d8#file-vgg_ilsvrc_16_layers_deploy-prototxt
-wget http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_16_layers.caffemodel
-cd ../../..
