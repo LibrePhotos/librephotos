5d5e438a84f2d243323136c43bc929e1bc4f2a88
Niaz Faridani-Rad <faridani.rad@gmail.com>
2021-02-02 15:51:20 +0100

    [FIX] Memory Leak

--- api/yolo/object_detection.py
+++ api/yolo/object_detection.py
@@ -1,7 +1,11 @@
 from darknetpy.detector import Detector
+import gc
 
 def detect_objects(image_path):
     detector = Detector('/code/api/yolo/yolo-9000/darknet/cfg/combine9k.data',
                         '/code/api/yolo/yolo-9000/darknet/cfg/yolo9000.cfg', 
                         '/code/api/yolo/yolo-9000/yolo9000-weights/yolo9000.weights')
-    return detector.detect(image_path)
+    result = detector.detect(image_path)
+    #kill darknet yolo process
+    gc.collect()
+    return result
