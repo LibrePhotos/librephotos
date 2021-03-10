8303f01a16831447f54c29b1612660c0b71aceaf
tomamplius <thomas@lgy.fr>
2021-03-10 22:27:27 +0100

    Use sql to improve build_social_graph performance

--- api/social_graph.py
+++ api/social_graph.py
@@ -3,21 +3,28 @@ from api.models import Person, Photo
 import itertools
 from django.db.models import Count
 from django.db.models import Q
+from django.db import connection
 
 def build_social_graph(user):
+    query = """
+        with face as (
+        	select photo_id,  person_id, name
+        	from api_face join api_person on api_person.id = person_id
+        	where person_label_is_inferred = false
+        )
+        select f1.name, f2.name
+        from face f1 join face f2 using (photo_id)
+        where f1.person_id != f2.person_id
+        group by f1.name, f2.name
+    """
     G = nx.Graph()
-
-    people = list(Person.objects.filter(Q(faces__photo__hidden=False) & Q(faces__photo__owner=user)).distinct().annotate(face_count=Count('faces')).filter(Q(face_count__gt=0)).order_by('-face_count'))[1:]
-    for person in people:
-        person = Person.objects.prefetch_related('faces__photo__faces__person').filter(id=person.id)[0]
-        for this_person_face in person.faces.all():
-            for other_person_face in this_person_face.photo.faces.all():
-                G.add_edge(person.name,other_person_face.person.name)
-    pos = nx.spring_layout(G,scale=1000,iterations=20)
-    nodes = [{'id':node,'x':pos[0],'y':pos[1]} for node,pos in pos.items()]
-    links = [{'source':pair[0], 'target':pair[1]} for pair in G.edges()]
-    res = {"nodes":nodes, "links":links}
-    return res
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        for link in cursor.fetchall():
+            G.add_edge(link[0],link[1])
+    pos = nx.spring_layout(G, k=1/2, scale=1000, iterations=20)
+    return { "nodes" : [{'id':node,'x':pos[0],'y':pos[1]} for node,pos in pos.items()],
+             "links" : [{'source':pair[0], 'target':pair[1]} for pair in G.edges()] }
 
 def build_ego_graph(person_id):
     G = nx.Graph()
