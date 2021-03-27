9ac07181cf8e4fef8323df79a951a37266b187ac
Thomas Legay <thomas@lgy.fr>
2021-03-28 00:18:06 +0100

    places365 limit reload model & allow unload (about #93)

--- api/places365/places365.py
+++ api/places365/places365.py
@@ -2,7 +2,6 @@
 # by Bolei Zhou, sep 2, 2017
 # last modified date: Dec. 27, 2017, migrating everything to python36 and latest pytorch and torchvision
 import os
-
 import ownphotos.settings
 import numpy as np
 import torch
@@ -15,44 +14,72 @@ from torchvision import transforms as trn
 
 # import warnings
 
-
 torch.nn.Module.dump_patches = True
 dir_places365_model = ownphotos.settings.PLACES365_ROOT
 
+class Places365():
+  labels_and_model_are_load = False
 
-def load_labels():
+  def unload(self):
+     self.model = None
+     self.classes = None
+     self.W_attribute = None
+     self.labels_IO = None
+     self.labels_attribute = None
+     self.labels_and_model_are_load = False
+
+  def load(self):
+    self.load_model()
+    self.load_labels()
+    self.labels_and_model_are_load = True
+
+  def load_model(self):  
+       # this model has a last conv feature map as 14x14
+       def hook_feature(module, input, output):
+           self.features_blobs.append(np.squeeze(output.data.cpu().numpy()))
+
+       model_file = os.path.join(dir_places365_model,'wideresnet18_places365.pth.tar')
+       self.model = wideresnet.resnet18(num_classes=365)
+       checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage)
+       state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+       self.model.load_state_dict(state_dict)
+       self.model.eval()
+       # hook the feature extractor
+       features_names = ['layer4','avgpool'] # this is the last conv layer of the resnet
+       for name in features_names:
+           self.model._modules.get(name).register_forward_hook(hook_feature)
+
+  def load_labels(self):
     # prepare all the labels
     # scene category relevant
     file_path_category = os.path.join(dir_places365_model,'categories_places365.txt')
-    classes = list()
+    self.classes = list()
     with open(file_path_category) as class_file:
         for line in class_file:
-            classes.append(line.strip().split(' ')[0][3:])
-    classes = tuple(classes)
+            self.classes.append(line.strip().split(' ')[0][3:])
+    self.classes = tuple(self.classes)
 
     # indoor and outdoor relevant
     file_path_IO = os.path.join(dir_places365_model,'IO_places365.txt')
     with open(file_path_IO) as f:
         lines = f.readlines()
-        labels_IO = []
+        self.labels_IO = []
         for line in lines:
             items = line.rstrip().split()
-            labels_IO.append(int(items[-1]) -1) # 0 is indoor, 1 is outdoor
-    labels_IO = np.array(labels_IO)
+            self.labels_IO.append(int(items[-1]) -1) # 0 is indoor, 1 is outdoor
+    self.labels_IO = np.array(self.labels_IO)
 
     # scene attribute relevant
     file_path_attribute = os.path.join(dir_places365_model,'labels_sunattribute.txt')
     with open(file_path_attribute) as f:
         lines = f.readlines()
-        labels_attribute = [item.rstrip() for item in lines]
+        self.labels_attribute = [item.rstrip() for item in lines]
 
     file_path_W = os.path.join(dir_places365_model,'W_sceneattribute_wideresnet18.npy')
-    W_attribute = np.load(file_path_W)
+    self.W_attribute = np.load(file_path_W)
+    self.labels_are_load = True
 
-    return classes, labels_IO, labels_attribute, W_attribute
-
-
-def returnTF():
+  def returnTF(self):
 # load the image transformer
     tf = trn.Compose([
         trn.Resize((224,224)),
@@ -61,47 +88,27 @@ def returnTF():
     ])
     return tf
 
-def remove_nonspace_separators(text):
+  def remove_nonspace_separators(self,text):
     return ' '.join(' '.join(' '.join(text.split('_')).split('/')).split('-'))
 
-
-# load the labels
-classes, labels_IO, labels_attribute, W_attribute = load_labels()
-
-def inference_places365(img_path, confidence):
+  def inference_places365(self,img_path, confidence):
     """
     @param img_path: path to the image to generate labels from
     @param confidence: minimum confidence before an category is selected
     @return: {'environment': 'indoor'/'outdoor', 'categories': [...], 'attributes': [...]}
     """
     try:
-
-        def hook_feature(module, input, output):
-            features_blobs.append(np.squeeze(output.data.cpu().numpy()))
-
-        def load_model():  # TODO Should the model be reloaded for every photo? Wouldn't it be better to do that once?
-            # this model has a last conv feature map as 14x14
-            model_file = os.path.join(dir_places365_model,'wideresnet18_places365.pth.tar')
-            model = wideresnet.resnet18(num_classes=365)
-            checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage)
-            state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
-            model.load_state_dict(state_dict)
-            model.eval()
-            # hook the feature extractor
-            features_names = ['layer4','avgpool'] # this is the last conv layer of the resnet
-            for name in features_names:
-                model._modules.get(name).register_forward_hook(hook_feature)
-            return model
+        if not self.labels_and_model_are_load:
+            self.load()
 
         # load the model
-        features_blobs = []
-        model = load_model()
+        self.features_blobs = []
 
         # load the transformer
-        tf = returnTF() # image transformer
+        tf = self.returnTF() # image transformer
 
         # get the softmax weight
-        params = list(model.parameters())
+        params = list(self.model.parameters())
         weight_softmax = params[-2].data.numpy()
         weight_softmax[weight_softmax<0] = 0
 
@@ -113,7 +120,7 @@ def inference_places365(img_path, confidence):
         input_img = V(tf(img).unsqueeze(0))
 
         # forward pass
-        logit = model.forward(input_img)
+        logit = self.model.forward(input_img)
         h_x = F.softmax(logit, 1).data.squeeze()
         probs, idx = h_x.sort(0, True)
         probs = probs.numpy()
@@ -124,7 +131,7 @@ def inference_places365(img_path, confidence):
         # output the IO prediction
         # labels_IO[idx[:10]] returns a list of 0's and 1's: 0 -> inside, 1 -> outside
         # Determine the mean to reach a consensus
-        io_image = np.mean(labels_IO[idx[:10]])
+        io_image = np.mean(self.labels_IO[idx[:10]])
         if io_image < 0.5:
             res['environment'] = 'indoor'
         else:
@@ -137,7 +144,7 @@ def inference_places365(img_path, confidence):
         res['categories'] = []
         for i in range(0, 5):
             if probs[i] > confidence:
-                res['categories'].append(remove_nonspace_separators(classes[idx[i]]))
+                res['categories'].append(self.remove_nonspace_separators(self.classes[idx[i]]))
             else:
                 break
         # TODO Should be replaced with more meaningful tags in the future
@@ -148,12 +155,14 @@ def inference_places365(img_path, confidence):
         # And sort it along the -1 axis
         # This results in idx_a, with the last elements the index numbers of attributes, we have the most confidence in
         # Can't seem to get any confidence values, also all the attributes it detect are not really meaningful i.m.o.
-        responses_attribute = W_attribute.dot(features_blobs[1])
+        responses_attribute = self.W_attribute.dot(self.features_blobs[1])
         idx_a = np.argsort(responses_attribute)
         res['attributes'] = []
         for i in range(-1, -10, -1):
-           res['attributes'].append(remove_nonspace_separators(labels_attribute[idx_a[i]]))
+           res['attributes'].append(self.remove_nonspace_separators(self.labels_attribute[idx_a[i]]))
 
         return res
     except Exception:
         logger.exception("Error:")
+
+place365_instance = Places365()
