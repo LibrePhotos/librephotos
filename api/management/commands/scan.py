1a19a42747bb4c45a2e7711428acb751913eb35f
Thomas Legay <thomas@lgy.fr>
2021-05-05 00:37:08 +0200

    Add owncloud photo on scan command

--- api/management/commands/scan.py
+++ api/management/commands/scan.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from api.models import User
 from api.directory_watcher import scan_photos
+from nextcloud.directory_watcher import scan_photos as scan_photos_cloud
 from api.util import logger
 import uuid
 import django_rq
@@ -9,4 +10,10 @@ class Command(BaseCommand):
     help = 'scan directory for all users'
     def handle(self, *args, **kwargs):
         for user in User.objects.all():
-            scan_photos(user,uuid.uuid4())
+            print("scan user : " + user.username)
+            if user.scan_directory != None and user.scan_directory != "" :
+                print("Scan folder : " + user.scan_directory)
+                scan_photos(user,uuid.uuid4())
+            if user.nextcloud_server_address != None and user.nextcloud_server_address != "":
+                print("Scan cloud url : " + user.scan_directory)
+                scan_photos_cloud(user,uuid.uuid4())
