2963f447a3bdce978d5e5d0466e826176cd8abe0
Niaz Faridani-Rad <faridani.rad@gmail.com>
2021-05-22 13:04:09 +0200

    [FIX] Add migration

---
+++ api/migrations/0005_add_video_to_photo.py
@@ -0,0 +1,15 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0004_fix_album_thing_constraint'),
+    ]
+
+    operations = [    
+        migrations.AddField(
+            model_name='Photo',
+            name='video',
+            field= models.BooleanField(default=False)
+        )
+    ]
