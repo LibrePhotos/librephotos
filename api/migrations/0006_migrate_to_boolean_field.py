483a90bc61196364b2d37ea7749e501cb9de50b7
Niaz Faridani-Rad <faridani.rad@gmail.com>
2021-05-25 11:43:00 +0200

    [REF] Migrate to Boolean Field

---
+++ api/migrations/0006_migrate_to_boolean_field.py
@@ -0,0 +1,15 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0005_add_video_to_photo'),
+    ]
+
+    operations = [    
+        migrations.AlterField(
+            model_name='Face',
+            name='person_label_is_inferred',
+            field=models.BooleanField(null= True, db_index=True)
+        )
+    ]
