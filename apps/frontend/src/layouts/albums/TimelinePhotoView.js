d19f1ac36924794137f67b4184fa484150ec986b
Niaz Faridani-Rad <faridani.rad@gmail.com>
2021-06-20 12:15:26 +0200

    [FIX] Invalid date when viewed second time

--- apps/frontend/src/layouts/albums/TimelinePhotoView.js
+++ apps/frontend/src/layouts/albums/TimelinePhotoView.js
@@ -1,35 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchDateAlbumsPhotoHashList } from '../../actions/albumsActions'
-import { PhotoListView } from '../../components/photolist/PhotoListView'
+import { fetchDateAlbumsPhotoHashList } from "../../actions/albumsActions";
+import { PhotoListView } from "../../components/photolist/PhotoListView";
 import * as moment from "moment";
 
 export class TimelinePhotoView extends Component {
-    componentDidMount() {
-        if (this.props.albumsDatePhotoHashList.length < 1) {
-            this.props.dispatch(fetchDateAlbumsPhotoHashList())
-        }
-    }
 
-    render() {
-        // This will get changed every time it is called...
-        const {fetchingAlbumsDatePhotoHashList} = this.props
-        var changedStuff = this.props.albumsDatePhotoHashList
-        changedStuff.forEach(group => group.date = moment(group.date ).format("MMM Do YYYY, dddd"))
-        return (
-            <PhotoListView 
-                title={"Photos"}
-                loading={fetchingAlbumsDatePhotoHashList}
-                titleIconName={'images'}
-                isDateView={true}
-                photosGroupedByDate={changedStuff}
-                idx2hash={this.props.albumsDatePhotoHashList.flatMap((el)=>el.items)}
-            />
-        )
+  componentDidMount() {
+    if (this.props.albumsDatePhotoHashList.length < 1) {
+      this.props.dispatch(fetchDateAlbumsPhotoHashList());
     }
+  }
+
+  render() {
+    // This will get changed every time it is called...
+    const { fetchingAlbumsDatePhotoHashList } = this.props;
+    const changedStuff = this.props.albumsDatePhotoHashList;
+    changedStuff.forEach(
+      (group) => (group.date = moment(group.date).format("MMM Do YYYY, dddd") !== "Invalid date" ?  moment(group.date).format("MMM Do YYYY, dddd") : group.date)
+    );
+    return (
+      <PhotoListView
+        title={"Photos"}
+        loading={fetchingAlbumsDatePhotoHashList}
+        titleIconName={"images"}
+        isDateView={true}
+        photosGroupedByDate={changedStuff}
+        idx2hash={this.props.albumsDatePhotoHashList.flatMap((el) => el.items)}
+      />
+    );
+  }
 }
 
-TimelinePhotoView = connect((store)=>{
+TimelinePhotoView = connect((store) => {
   return {
     showSidebar: store.ui.showSidebar,
 
@@ -44,7 +47,8 @@ TimelinePhotoView = connect((store)=>{
     idx2hash: store.albums.idx2hash,
 
     albumsDatePhotoHashList: store.albums.albumsDatePhotoHashList,
-    fetchingAlbumsDatePhotoHashList: store.albums.fetchingAlbumsDatePhotoHashList,
-    fetchedAlbumsDatePhotoHashList: store.albums.fetchedAlbumsDatePhotoHashList,    
-  }
-})(TimelinePhotoView)
+    fetchingAlbumsDatePhotoHashList:
+      store.albums.fetchingAlbumsDatePhotoHashList,
+    fetchedAlbumsDatePhotoHashList: store.albums.fetchedAlbumsDatePhotoHashList,
+  };
+})(TimelinePhotoView);
