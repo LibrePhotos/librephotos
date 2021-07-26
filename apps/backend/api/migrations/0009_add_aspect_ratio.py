9586b34585554df084942f48c392e1420493156b
Niaz Faridani-Rad <faridani.rad@gmail.com>
2021-07-26 22:28:41 +0200

    Only calculate aspect ratio if there is a valid thumbnail

--- apps/backend/api/migrations/0009_add_aspect_ratio.py
+++ apps/backend/api/migrations/0009_add_aspect_ratio.py
@@ -11,13 +11,14 @@ class Migration(migrations.Migration):
         Photo = apps.get_model('api', 'Photo')
         with exiftool.ExifTool() as et:
             for obj in Photo.objects.all():
-                try:
-                    height = et.get_tag('ImageHeight', obj.thumbnail_big.path)
-                    width = et.get_tag('ImageWidth', obj.thumbnail_big.path)
-                    obj.aspect_ratio = round((width /height), 2)
-                    obj.save()
-                except:
-                    print('Cannot convert {} object'.format(obj.image_path))
+                if obj.thumbnail_big:
+                    try:
+                        height = et.get_tag('ImageHeight', obj.thumbnail_big.path)
+                        width = et.get_tag('ImageWidth', obj.thumbnail_big.path)
+                        obj.aspect_ratio = round((width /height), 2)
+                        obj.save()
+                    except:
+                        print('Cannot convert {} object'.format(obj.image_path))
 
     operations = [    
         migrations.AddField(model_name='Photo', name='aspect_ratio', field=models.FloatField(blank=True, null=True)),
