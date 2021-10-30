9e2dec8161f89776640eaf0c28aa96791873ab9c
Niaz Faridani-Rad <faridani.rad@gmail.com>
2021-10-30 16:19:36 +0200

    Add experimental trancoding option

---
+++ api/migrations/0016_add_transcode_videos.py
@@ -0,0 +1,15 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("api", "0015_add_dominant_color"),
+    ]
+    operations = [
+        migrations.AddField(
+            model_name="User",
+            name="transcode_videos",
+            field=models.BooleanField(default=False),
+        ),
+    ]
