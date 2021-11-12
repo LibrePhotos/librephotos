b5a97584b6eb99afd8bb24b44ef62d35923098c2
Akshay Naik <akshay9_5@yahoo.co.in>
2021-11-13 01:00:01 +0530

    Optimize Frontend Docker Image

--- deploy/frontend/entrypoint.sh
+++ deploy/frontend/entrypoint.sh
@@ -2,14 +2,6 @@
 
 echo "installing frontend"
 npm install --legacy-peer-deps
+npm run start
 
-echo "serving frontend"
-if [ "$DEBUG" = 1 ]
-then
-    echo "develompent running frontend"
-    npm run start
-else
-    echo "productions running frontend"
-    serve build -d -l 3000
-fi
 # DANGEROUSLY_DISABLE_HOST_CHECK=true HOST=0.0.0.0 npm start
