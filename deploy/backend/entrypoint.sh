ff733612f807ef129a0425e1a90d689871c53c6c
Niaz Faridani-Rad <faridani.rad@gmail.com>
2021-11-18 19:16:57 +0100

    Remove build index from entrypoint

--- deploy/backend/entrypoint.sh
+++ deploy/backend/entrypoint.sh
@@ -6,8 +6,7 @@ python image_similarity/main.py 2>&1 | tee /logs/gunicorn_image_similarity.log &
 python manage.py showmigrations | tee /logs/show_migrate.log
 python manage.py migrate | tee /logs/command_migrate.log
 python manage.py showmigrations | tee /logs/show_migrate.log
-python manage.py build_similarity_index 2>&1 | tee /logs/command_build_similarity_index.log
-python manage.py clear_cache
+python manage.py clear_cache 
 python manage.py createadmin -u $ADMIN_USERNAME $ADMIN_EMAIL 2>&1 | tee /logs/command_createadmin.log
 
 echo "Running backend server..."
