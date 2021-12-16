f5b20e7cae5d816e0526009e1a97948725a1ac06
Niaz Faridani-Rad <faridani.rad@gmail.com>
2021-12-16 23:34:39 +0100

    Handle semantic search
    If semantic search is activated, we should display the flat view instead.
    Semantic search returns value per relevance.

--- apps/frontend/src/layouts/SearchView.js
+++ apps/frontend/src/layouts/SearchView.js
@@ -11,16 +11,21 @@ export class SearchView extends Component {
       // User hasn't searched for anything. Redirect to root.
       return <Redirect to={"/"} />;
     }
-
+    //if semantic search is activated we get a flat array sorted by relevance
+    //thats why we have to change the parameters
     const title = `Searching "${this.props.searchQuery}"...`;
     return (
       <PhotoListView
         title={title}
         loading={this.props.fetchedPhotosetType !== PhotosetType.SEARCH}
         titleIconName={"search"}
-        isDateView={true}
+        isDateView={this.props.user.semantic_search_topk == 0}
         photoset={this.props.photosGroupedByDate}
-        idx2hash={this.props.photosFlat}
+        idx2hash={
+          this.props.user.semantic_search_topk == 0
+            ? this.props.photosFlat
+            : this.props.photosGroupedByDate
+        }
         selectable={true}
       />
     );
@@ -29,6 +34,7 @@ export class SearchView extends Component {
 
 SearchView = connect((store) => {
   return {
+    user: store.user.userSelfDetails,
     photosFlat: store.photos.photosFlat,
     photosGroupedByDate: store.photos.photosGroupedByDate,
     fetchedPhotosetType: store.photos.fetchedPhotosetType,
