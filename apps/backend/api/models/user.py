19a003644ef1b2ddd7c688f1912a1a2a702dccc2
derneuere <faridani.rad@gmail.com>
2022-01-24 18:15:47 +0100

    Add new field to serializer
    - rename migration
    - add new function

--- apps/backend/api/models/user.py
+++ apps/backend/api/models/user.py
@@ -1,10 +1,15 @@
+import json
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django_cryptography.fields import encrypt
 
 import ownphotos.settings
-from api.date_time_extractor import DEFAULT_RULES_PARAMS
+from api.date_time_extractor import DEFAULT_RULES
 
+
+def get_default_config_datetime_rules():  # This is a callable
+    return json.dumps(DEFAULT_RULES, default=lambda x: x.__dict__) 
 
 class User(AbstractUser):
     scan_directory = models.CharField(max_length=512, db_index=True)
@@ -32,11 +37,13 @@ class User(AbstractUser):
     save_metadata_to_disk = models.TextField(
         choices=SaveMetadataToDisk.choices, default=SaveMetadataToDisk.OFF
     )
-    config_datetime_rules = models.JSONField(default=json.dumps(DEFAULT_RULES_PARAMS))
+
+    config_datetime_rules = models.JSONField(default=get_default_config_datetime_rules)
 
 
 def get_admin_user():
     return User.objects.get(is_superuser=True)
+
 
 
 def get_deleted_user():
