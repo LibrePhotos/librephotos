3b7490386fb31546f662d787cb4c155783d90dd9
Niaz <tigrexspalterlp@gmail.com>
2022-02-21 18:58:20 +0100

    Don't check if data is serializable

--- apps/mobile/src/Store/index.js
+++ apps/mobile/src/Store/index.js
@@ -50,9 +50,7 @@ const store = configureStore({
   reducer: persistedReducer,
   middleware: getDefaultMiddleware => {
     const middlewares = getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
+      serializableCheck: false,
     })
 
     middlewares.push(thunk)
