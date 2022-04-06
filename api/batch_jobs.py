657eb1b984f48d510068d1f7a805dc324732278b
derneuere <faridani.rad@gmail.com>
2022-04-07 00:04:44 +0200

    Fix multiple issues with calculate clip embeddings
    - only calculate embeddings for the user how asked for it
    - log the exception
    - only run as many threads as specified in heavyweight_process

--- api/batch_jobs.py
+++ api/batch_jobs.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 import pytz
 import torch
+from constance import config as site_config
+from django.db.models import Q
 from django_rq import job
 
 import api.util as util
@@ -33,9 +35,15 @@ def batch_calculate_clip_embedding(job_id, user):
     lrj = LongRunningJob.objects.get(job_id=job_id)
     lrj.started_at = datetime.now().replace(tzinfo=pytz.utc)
 
-    count = Photo.objects.filter(clip_embeddings__isnull=True).count()
+    count = Photo.objects.filter(
+        Q(owner=user) & Q(clip_embeddings__isnull=True)
+    ).count()
     lrj.result = {"progress": {"current": 0, "target": count}}
     lrj.save()
+
+    num_threads = max(1, site_config.HEAVYWEIGHT_PROCESS)
+    torch.set_num_threads(num_threads)
+    os.environ["OMP_NUM_THREADS"] = str(num_threads)
 
     BATCH_SIZE = 64
     util.logger.info("Using threads: {}".format(torch.get_num_threads()))
@@ -43,7 +51,11 @@ def batch_calculate_clip_embedding(job_id, user):
     try:
         done_count = 0
         while done_count < count:
-            objs = list(Photo.objects.filter(clip_embeddings__isnull=True)[:BATCH_SIZE])
+            objs = list(
+                Photo.objects.filter(Q(owner=user) & Q(clip_embeddings__isnull=True))[
+                    :BATCH_SIZE
+                ]
+            )
             valid_objs = []
             for obj in objs:
                 if os.path.exists(obj.thumbnail_big.path):
@@ -72,4 +84,4 @@ def batch_calculate_clip_embedding(job_id, user):
         lrj.save()
 
     except Exception as e:
-        print(e)
+        util.logger.error("Error in batch_calculate_clip_embedding: {}".format(e))
