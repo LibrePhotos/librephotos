4c2b2303f0b19d6c6c89f5290301d13d8d9c04bb
derneuere <faridani.rad@gmail.com>
2022-04-13 00:06:48 +0200

    Check a range when adding new photos to auto album
    - also try catch generating titles

--- api/models/album_auto.py
+++ api/models/album_auto.py
@@ -25,72 +25,79 @@ class AlbumAuto(models.Model):
         unique_together = ("timestamp", "owner")
 
     def _generate_title(self):
-        weekday = ""
-        time = ""
-        loc = ""
-        if self.timestamp:
-            weekday = util.weekdays[self.timestamp.isoweekday()]
-            hour = self.timestamp.hour
-            if hour > 0 and hour < 5:
-                time = "Early Morning"
-            elif hour >= 5 and hour < 12:
-                time = "Morning"
-            elif hour >= 12 and hour < 18:
-                time = "Afternoon"
-            elif hour >= 18 and hour <= 24:
-                time = "Evening"
+        try:
+            weekday = ""
+            time = ""
+            loc = ""
+            if self.timestamp:
+                weekday = util.weekdays[self.timestamp.isoweekday()]
+                hour = self.timestamp.hour
+                if hour > 0 and hour < 5:
+                    time = "Early Morning"
+                elif hour >= 5 and hour < 12:
+                    time = "Morning"
+                elif hour >= 12 and hour < 18:
+                    time = "Afternoon"
+                elif hour >= 18 and hour <= 24:
+                    time = "Evening"
 
-        when = " ".join([weekday, time])
+            when = " ".join([weekday, time])
 
-        photos = self.photos.all()
+            photos = self.photos.all()
 
-        loc = ""
-        pep = ""
+            loc = ""
+            pep = ""
 
-        places = []
-        people = []
-        timestamps = []
+            places = []
+            people = []
+            timestamps = []
 
-        for photo in photos:
-            if photo.geolocation_json and "features" in photo.geolocation_json.keys():
-                for feature in photo.geolocation_json["features"]:
-                    if feature["place_type"][0] == "place":
-                        places.append(feature["text"])
+            for photo in photos:
+                if (
+                    photo.geolocation_json
+                    and "features" in photo.geolocation_json.keys()
+                ):
+                    for feature in photo.geolocation_json["features"]:
+                        if feature["place_type"][0] == "place":
+                            places.append(feature["text"])
 
-            timestamps.append(photo.exif_timestamp)
+                timestamps.append(photo.exif_timestamp)
 
-            faces = photo.faces.all()
-            for face in faces:
-                people.append(face.person.name)
+                faces = photo.faces.all()
+                for face in faces:
+                    people.append(face.person.name)
 
-        if len(places) > 0:
-            cnts_places = Counter(places)
-            loc = "in " + " and ".join(dict(cnts_places.most_common(2)).keys())
-        if len(people) > 0:
-            cnts_people = Counter(people)
-            names = dict(
-                [
-                    (k, v)
-                    for k, v in cnts_people.most_common(2)
-                    if k.lower() != "unknown"
-                ]
-            ).keys()
-            if len(names) > 0:
-                pep = "with " + " and ".join(names)
-        if len(timestamps) > 0:
-            if (max(timestamps) - min(timestamps)).days >= 3:
-                when = "%d days" % ((max(timestamps) - min(timestamps)).days)
+            if len(places) > 0:
+                cnts_places = Counter(places)
+                loc = "in " + " and ".join(dict(cnts_places.most_common(2)).keys())
+            if len(people) > 0:
+                cnts_people = Counter(people)
+                names = dict(
+                    [
+                        (k, v)
+                        for k, v in cnts_people.most_common(2)
+                        if k.lower() != "unknown"
+                    ]
+                ).keys()
+                if len(names) > 0:
+                    pep = "with " + " and ".join(names)
+            if len(timestamps) > 0:
+                if (max(timestamps) - min(timestamps)).days >= 3:
+                    when = "%d days" % ((max(timestamps) - min(timestamps)).days)
 
-            weekend = [5, 6]
-            if (
-                max(timestamps).weekday() in weekend
-                and min(timestamps).weekday() in weekend
-                and not (max(timestamps).weekday() == min(timestamps).weekday())
-            ):
-                when = "Weekend"
+                weekend = [5, 6]
+                if (
+                    max(timestamps).weekday() in weekend
+                    and min(timestamps).weekday() in weekend
+                    and not (max(timestamps).weekday() == min(timestamps).weekday())
+                ):
+                    when = "Weekend"
 
-        title = " ".join([when, pep, loc]).strip()
-        self.title = title
+            title = " ".join([when, pep, loc]).strip()
+            self.title = title
+            self.save()
+        except Exception as e:
+            util.logger.exception(e)
 
     def __str__(self):
         return "%d: %s" % (self.id, self.title)
