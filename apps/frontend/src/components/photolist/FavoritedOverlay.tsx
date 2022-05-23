2cdf0249debfbf97e88dfa5c3f58e9e0818be1de
derneuere <faridani.rad@gmail.com>
2022-05-24 00:14:22 +0200

    Fix favorite star

--- apps/frontend/src/components/photolist/FavoritedOverlay.tsx
+++ apps/frontend/src/components/photolist/FavoritedOverlay.tsx
@@ -1,11 +1,9 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
 import { Star } from "tabler-icons-react";
 
 import { useAppSelector } from "../../store/store";
 
 export const FavoritedOverlay = (item: any) => {
-  const favorite_min_rating = useAppSelector(store => store.user.userSelfDetails.favorite_min_rating);
-
-  return <span>{item.rating >= favorite_min_rating && <Star color="yellow" />}</span>;
+  const { favorite_min_rating } = useAppSelector(store => store.user.userSelfDetails);
+  return item.item.rating >= favorite_min_rating && <Star strokeWidth={3} style={{ marginRight: 4 }} color="#FFD700" />;
 };
