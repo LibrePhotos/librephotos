a542ea5e09ff6d96cef9ee67ece45285039f91de
derneuere <faridani.rad@gmail.com>
2022-07-02 15:54:32 +0200

    Migrate SharedToMe to TypeScript and Mantine

--- apps/frontend/src/layouts/sharing/SharedToMe.tsx
+++ apps/frontend/src/layouts/sharing/SharedToMe.tsx
@@ -1,8 +1,8 @@
 import { Group, Stack, Tabs, Text, Title } from "@mantine/core";
 import React, { useEffect, useState } from "react";
 
-import { fetchUserAlbumsSharedFromMe } from "../../actions/albumsActions";
-import { fetchPhotosSharedFromMe } from "../../actions/photosActions";
+import { fetchUserAlbumsSharedToMe } from "../../actions/albumsActions";
+import { fetchPhotosSharedToMe } from "../../actions/photosActions";
 import { fetchPublicUserList } from "../../actions/publicActions";
 import { PhotosetType } from "../../reducers/photosReducer";
 import { useAppDispatch, useAppSelector } from "../../store/store";
@@ -13,18 +13,18 @@ type Props = {
   match: any;
 };
 
-export const SharedFromMe = (props: Props) => {
+export const SharedToMe = (props: Props) => {
   const dispatch = useAppDispatch();
   const { albums } = useAppSelector(store => store);
   const { photosFlat, photosGroupedByUser, fetchedPhotosetType } = useAppSelector(store => store.photos);
   const { match } = props;
   const [activeTab, setActiveTab] = useState(1);
-  
+
   useEffect(() => {
     if (fetchedPhotosetType !== PhotosetType.SHARED_BY_ME) {
       dispatch(fetchPublicUserList());
-      dispatch(fetchPhotosSharedFromMe());
-      dispatch(fetchUserAlbumsSharedFromMe());
+      dispatch(fetchPhotosSharedToMe());
+      dispatch(fetchUserAlbumsSharedToMe());
     }
   }, []);
 
@@ -32,20 +32,26 @@ export const SharedFromMe = (props: Props) => {
     if (activeItem === "photos") {
       return (
         <Text color="dimmed">
-          {photosFlat.length} photo share(s) with {photosGroupedByUser.length} user(s)
+          {photosGroupedByUser.length} user(s) shared {photosFlat.length} photo(s) with you
         </Text>
       );
     }
-    return <Text color="dimmed">You shared {albums.albumsSharedFromMe.length} albums</Text>;
+    return (
+      <Text color="dimmed">
+        {albums.albumsSharedToMe.length} user(s) shared{" "}
+        {albums.albumsSharedToMe.length > 0 &&
+          albums.albumsSharedToMe.map(el => el.albums.length).reduce((a, b) => a + b, 0)}{" "}
+        album(s) with you
+      </Text>
+    );
   };
-
   const activeItem = match.params.which;
 
   return (
     <Stack>
       <Group>
         <div>
-          <Title order={2}> {activeItem === "photos" ? "Photos" : "Albums"} you shared</Title>
+          <Title order={2}> {activeItem === "photos" ? "Photos" : "Albums"} others shared </Title>
           <Text color="dimmed" size="sm">
             {getSubHeader(activeItem)}
           </Text>
@@ -53,10 +59,10 @@ export const SharedFromMe = (props: Props) => {
       </Group>
       <Tabs active={activeTab} onTabChange={setActiveTab}>
         <Tabs.Tab label="Photos" name="photos">
-          <PhotosShared isSharedToMe={false} />
+          <PhotosShared isSharedToMe />
         </Tabs.Tab>
         <Tabs.Tab label="Albums" name="albums">
-          <AlbumsShared isSharedToMe={false} />
+          <AlbumsShared isSharedToMe />
         </Tabs.Tab>
       </Tabs>
     </Stack>
