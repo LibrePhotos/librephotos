5aa5e5a7bd0b07f7a3846a4c9d34a5844bcba9da
derneuere <faridani.rad@gmail.com>
2022-07-06 18:13:19 +0200

    Add the option to change the cover photos of user albums

---
+++ api/migrations/0025_add_cover_photo.py
@@ -0,0 +1,21 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("api", "0024_photo_timestamp"),
+    ]
+    operations = [
+        migrations.AddField(
+            model_name="AlbumUser",
+            name="cover_photo",
+            field=models.ForeignKey(
+                to="api.Photo",
+                related_name="album_user",
+                on_delete=models.PROTECT,
+                blank=False,
+                null=True,
+            ),
+        ),
+    ]
