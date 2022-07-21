1ef46ed671414c39118aa3a8418e4db14a5ada7e
Daniel Einars <contact@dle.dev>
2022-07-21 09:11:07 +0200

    add configuration for wdyr
    Signed-off-by: Daniel Einars <contact@dle.dev>

--- apps/frontend/src/wdyr.ts
+++ apps/frontend/src/wdyr.ts
@@ -1,13 +1,14 @@
 /// <reference types="@welldone-software/why-did-you-render" />
 import React from "react";
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development" && process.env.REACT_APP_WDYR === "true") {
   // eslint-disable-next-line import/no-extraneous-dependencies,global-require
   const whyDidYouRender = require("@welldone-software/why-did-you-render");
   // eslint-disable-next-line global-require
   const ReactRedux = require("react-redux");
   whyDidYouRender(React, {
     trackAllPureComponents: true,
+    trackHooks: true,
     trackExtraHooks: [[ReactRedux, "useAppSelector"]],
   });
 }
