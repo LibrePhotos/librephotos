8030128180353607c8ab9f4eb16bef78703409be
Niaz Faridani-Rad <faridani.rad@gmail.com>
2022-08-18 13:24:41 +0200

    Remove long time out
    - Any API call that takes longer than 30 seconds should be refactored to use a background worker instead.

--- deploy/backend/entrypoint.sh
+++ deploy/backend/entrypoint.sh
@@ -37,8 +37,8 @@ python manage.py rqworker default 2>&1 | tee /logs/rqworker.log &
 if [ "$DEBUG" = 1 ]
 then
     echo "development backend starting"
-    gunicorn --worker-class=gevent --timeout 36000 --reload --bind 0.0.0.0:8001 --log-level=info ownphotos.wsgi 2>&1 | tee /logs/gunicorn_django.log
+    gunicorn --worker-class=gevent --reload --bind 0.0.0.0:8001 --log-level=info ownphotos.wsgi 2>&1 | tee /logs/gunicorn_django.log
 else
     echo "production backend starting"
-    gunicorn --worker-class=gevent --timeout 3600 --bind 0.0.0.0:8001 --log-level=info ownphotos.wsgi 2>&1 | tee /logs/gunicorn_django.log
+    gunicorn --worker-class=gevent --bind 0.0.0.0:8001 --log-level=info ownphotos.wsgi 2>&1 | tee /logs/gunicorn_django.log
 fi
