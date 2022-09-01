44361a5fb3bb9bf9ca7a6bef44bc6be9f255296a
derneuere <faridani.rad@gmail.com>
2022-09-01 13:20:56 +0200

    Remove generating secret.key on entrypoint

--- deploy/backend/entrypoint.sh
+++ deploy/backend/entrypoint.sh
@@ -2,23 +2,6 @@
 export PYTHONUNBUFFERED=TRUE
 export PYTHONFAULTHANDLER=1
 mkdir -p /logs
-if [ -n "$SECRET_KEY" ]
-then
-    echo "Use env SECRET_KEY"
-else 
-    if [ -f /logs/secret.key ]
-    then
-        echo "Use existing secret.key"
-        SECRET_KEY=`cat /logs/secret.key`
-        export SECRET_KEY=$SECRET_KEY
-    else
-        echo "Create new secret.key"
-        SECRET_KEY=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
-        echo $SECRET_KEY > /logs/secret.key
-        export SECRET_KEY=$SECRET_KEY
-    fi
-fi
-
 python image_similarity/main.py 2>&1 | tee /logs/gunicorn_image_similarity.log &
 python manage.py showmigrations | tee /logs/show_migrate.log
 python manage.py migrate | tee /logs/command_migrate.log
