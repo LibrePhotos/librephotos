c521f743bb897c7d4d88f490c1563d16db7e1c59
5mauggy <5m4u66y@gmail.com>
2022-10-17 15:04:24 +0200

    Replaced moment.js by luxon

--- apps/frontend/src/components/charts/LocationDurationStackedBar.tsx
+++ apps/frontend/src/components/charts/LocationDurationStackedBar.tsx
@@ -1,8 +1,9 @@
 import { Loader, Stack, Text, Title } from "@mantine/core";
-import moment from "moment";
+import { DateTime } from "luxon";
 import React, { useEffect, useState } from "react";
 import useDimensions from "react-cool-dimensions";
 import { useTranslation } from "react-i18next";
+import i18n from "../../i18n";
 
 import { fetchLocationTimeline } from "../../actions/utilActions";
 import { useAppDispatch, useAppSelector } from "../../store/store";
@@ -35,24 +36,35 @@ export function LocationDurationStackedBar() {
     if (!fetchedLocationTimeline) {
       fetchLocationTimeline(dispatch);
     }
-  }, [dispatch]); // Only run on first render
+  }, [dispatch, fetchedLocationTimeline]); // Only run on first render
+  
+  const getLocationFromToLabel = (loc: string | undefined, start: number | undefined, end: number | undefined ) => {
+    if (typeof start === 'number' && typeof end === 'number') { 
+      return t("locationstimeline.fromto",{
+        location: loc,
+        start: DateTime.fromSeconds(start).setLocale(i18n.resolvedLanguage.replace("_", "-")).toLocaleString({ year: 'numeric', month: 'short' }),
+        end: DateTime.fromSeconds(end).setLocale(i18n.resolvedLanguage.replace("_", "-")).toLocaleString({ year: 'numeric', month: 'short' })
+      });
+    }
+    return null;
+  };
 
   return (
     <Stack ref={observe}>
       <Title order={3}>{t("locationtimeline")}</Title>
       {!fetchedLocationTimeline && <Loader />}
-      {locationTimeline.length == 0 && fetchedLocationTimeline && <Text color="dimmed">{t("nodata")}</Text>}
+      {locationTimeline.length === 0 && fetchedLocationTimeline && <Text color="dimmed">{t("nodata")}</Text>}
       {fetchedLocationTimeline && (
         <div>
           <XYPlot width={width - 30} height={300} stackBy="x">
-            <XAxis tickFormat={(v: any) => moment.unix(locationTimeline[0].start + v).format("YYYY-MM")} />
+            <XAxis tickFormat={(v: any) => DateTime.fromSeconds(parseFloat(locationTimeline[0].start + v)).setLocale(i18n.resolvedLanguage.replace("_", "-")).toLocaleString({ year: 'numeric', month: '2-digit' })} />
 
             {locationTimeline.map((el: any) => (
               <HorizontalBarSeries
                 onValueMouseOver={(d: any, info: any) => {
                   setHintValue(d);
                 }}
-                style={{ fill: el.color, stroke: el.color }}
+                style={{ fill: el.color, stroke: el.color}}
                 data={[
                   {
                     y: 1,
@@ -67,11 +79,8 @@ export function LocationDurationStackedBar() {
 
             {hintValue && (
               <Hint value={hintValue}>
-                <Text>
-                  {hintValue.loc}
-                  {` from ${moment.unix(hintValue.start).format("YYYY-MM-DD")} to ${moment
-                    .unix(hintValue.end)
-                    .format("YYYY-MM-DD")}`}
+                <Text color="black">
+                  {getLocationFromToLabel(hintValue.loc, hintValue.start, hintValue.end)}
                 </Text>
               </Hint>
             )}
