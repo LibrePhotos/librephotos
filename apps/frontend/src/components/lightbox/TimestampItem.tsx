bc37f406401a94f2bd82508b35feb4a5c5bacb66
5mauggy <5m4u66y@gmail.com>
2022-10-23 16:06:37 +0200

    Add time edit to lightbox

--- apps/frontend/src/components/lightbox/TimestampItem.tsx
+++ apps/frontend/src/components/lightbox/TimestampItem.tsx
@@ -1,73 +1,98 @@
-import { ActionIcon, Button, Group, Text } from "@mantine/core";
-import { DatePicker } from "@mantine/dates";
+import { ActionIcon, Button, Group, Stack, Text } from "@mantine/core";
+import { DatePicker, TimeInput } from "@mantine/dates";
 import { DateTime } from "luxon";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import "react-virtualized/styles.css";
 // only needs to be imported once
 import { Calendar, Check, Edit, X } from "tabler-icons-react";
+import { useAppDispatch } from "../../store/store";
 import i18n from "../../i18n";
 
 import { editPhoto } from "../../actions/photosActions";
 
 type Props = {
   photoDetail: any;
-  dispatch: any;
 };
 
-export function TimestampItem(props: Props) {
-  const [timestamp, setTimestamp] = useState(new Date(props.photoDetail.exif_timestamp));
+export function TimestampItem({photoDetail}: Props) {
+  const [timestamp, setTimestamp] = useState(new Date(photoDetail.exif_timestamp));
   const [editMode, setEditMode] = useState(false);
 
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
 
-  const onChange = (date: Date) => {
+  const onChangeDate = (date: Date) => {
+    date.setHours(timestamp.getHours());
+    date.setMinutes(timestamp.getMinutes());
+    date.setSeconds(timestamp.getSeconds());
     setTimestamp(date);
   };
 
-  const onSubmit = (e: any) => {
-    // To-Do: Use the user defined timezone
-    const { photoDetail } = props;
+  const onChangeTime = (date: Date) => {
+    date.setDate(timestamp.getDate());
+    date.setMonth(timestamp.getMonth());
+    date.setFullYear(timestamp.getFullYear());
+    setTimestamp(date);
+  };
 
+
+  const onSaveDateTime = (e: any) => {
+    // To-Do: Use the user defined timezone
     photoDetail.exif_timestamp = timestamp.toISOString();
     const differentJson = { exif_timestamp: photoDetail.exif_timestamp };
-    props.dispatch(editPhoto(props.photoDetail.image_hash, differentJson));
+    dispatch(editPhoto(photoDetail.image_hash, differentJson));
     setEditMode(false);
   };
 
+  const onCancelDateTime = (e: any) => {
+    setTimestamp(new Date(photoDetail.exif_timestamp));
+    setEditMode(false);
+  }
+
   return (
     <Group>
-      <Calendar />
+
       {/* To-Do: Handle click on calender */}
       {editMode && (
-        <Group>
-          <DatePicker value={timestamp} onChange={onChange}></DatePicker>
-          <ActionIcon variant="light" color="green" onClick={onSubmit}>
-            <Check></Check>
-          </ActionIcon>
-          <ActionIcon variant="light" onClick={() => setEditMode(!editMode)} color="red">
-            <X></X>
-          </ActionIcon>
-        </Group>
+        <Stack>
+          <Group>
+            <Calendar />
+            <Text>{t("lightbox.sidebar.editdatetime")}</Text>
+          </Group>
+          <Stack>
+            <DatePicker locale={i18n.resolvedLanguage.replace("_", "-")} value={timestamp} onChange={onChangeDate}/>
+            <TimeInput withSeconds value={timestamp} onChange={onChangeTime}/>
+            <Group>
+              <ActionIcon variant="light" color="green" onClick={onSaveDateTime}>
+                <Check />
+              </ActionIcon>
+              <ActionIcon variant="light" onClick={onCancelDateTime} color="red">
+                <X />
+              </ActionIcon>
+            </Group>
+          </Stack>
+        </Stack>
       )}
       {!editMode && (
-        <div>
+        <Group>
+          <Calendar />
           <Button color="dark" variant="subtle" onClick={() => setEditMode(!editMode)} rightIcon={<Edit />}>
             <div>
-              {DateTime.fromISO(props.photoDetail.exif_timestamp).isValid
-                ? DateTime.fromISO(props.photoDetail.exif_timestamp).setLocale(i18n.resolvedLanguage.replace("_", "-")).toLocaleString(DateTime.DATE_MED)
+              {DateTime.fromISO(photoDetail.exif_timestamp).isValid
+                ? DateTime.fromISO(photoDetail.exif_timestamp).setLocale(i18n.resolvedLanguage.replace("_", "-")).toLocaleString(DateTime.DATE_MED)
                 : null}
               <Text size="xs" color="dimmed">
-              {DateTime.fromISO(props.photoDetail.exif_timestamp).isValid
-                  ? DateTime.fromISO(props.photoDetail.exif_timestamp).setLocale(i18n.resolvedLanguage.replace("_", "-")).toFormat('cccc, ')
+              {DateTime.fromISO(photoDetail.exif_timestamp).isValid
+                  ? DateTime.fromISO(photoDetail.exif_timestamp).setLocale(i18n.resolvedLanguage.replace("_", "-")).toFormat('cccc, ')
                   : null} 
-              {DateTime.fromISO(props.photoDetail.exif_timestamp).isValid
-                  ? DateTime.fromISO(props.photoDetail.exif_timestamp).setLocale(i18n.resolvedLanguage.replace("_", "-")).toLocaleString(DateTime.TIME_SIMPLE)
+              {DateTime.fromISO(photoDetail.exif_timestamp).isValid
+                  ? DateTime.fromISO(photoDetail.exif_timestamp).setLocale(i18n.resolvedLanguage.replace("_", "-")).toLocaleString(DateTime.TIME_SIMPLE)
                   : null}
               </Text>
             </div>
           </Button>
-        </div>
+        </Group>
       )}
       {
         // To-Do: Show timezone of image
