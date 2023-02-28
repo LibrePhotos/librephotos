2400306003e65c5b164e7f7db315eaabf0d20ac1
derneuere <faridani.rad@gmail.com>
2023-02-28 19:09:58 +0100

    Remove not used files

--- apps/mobile/src/Services/User/FetchOne.js
+++
@@ -1,9 +0,0 @@
-import api, { handleError } from '@/Services'
-
-export default async userId => {
-  if (!userId) {
-    return handleError({ message: 'User ID is required' })
-  }
-  const response = await api.get(`users/${userId}`)
-  return response.data
-}
--- apps/mobile/src/Store/User/index.js
+++
@@ -1,9 +0,0 @@
-import { buildSlice } from '@thecodingmachine/redux-toolkit-wrapper'
-import FetchOne from './FetchOne'
-
-// This state is common to all the "user" module, and can be modified by any "user" reducers
-const sliceInitialState = {
-  item: {},
-}
-
-export default buildSlice('user', [FetchOne], sliceInitialState).reducer
