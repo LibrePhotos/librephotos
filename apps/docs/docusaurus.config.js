7d17fcbaecc4a55fcea1952cd8c9f0903cbe2d8a
Niaz <tigrexspalterlp@gmail.com>
2023-03-03 12:18:55 +0100

    Allow editing pages

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -39,17 +39,13 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/LibrePhotos/librephotos.docs/tree/master/",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/LibrePhotos/librephotos.docs/tree/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
