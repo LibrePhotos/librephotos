c1f8146bd1c0de8e8786c9e6c246b2fdd8e8e84d
derneuere <faridani.rad@gmail.com>
2023-04-10 12:20:52 +0200

    Show more useful values in worker logs

--- apps/frontend/src/components/job/JobDuration.tsx
+++ apps/frontend/src/components/job/JobDuration.tsx
@@ -1,6 +1,7 @@
 import { DateTime } from "luxon";
 import React from "react";
 import { useTranslation } from "react-i18next";
+
 import i18n from "../../i18n";
 
 interface IJobDuration {
@@ -15,7 +16,15 @@ export function JobDuration({ matches, finished, finishedAt, startedAt }: IJobDu
 
   if (matches) {
     if (finished && finishedAt && startedAt) {
-      return <td>{DateTime.fromISO(finishedAt).diff(DateTime.fromISO(startedAt)).reconfigure({locale: i18n.resolvedLanguage.replace("_", "-")}).toHuman()}</td>;
+      return (
+        <td>
+          {DateTime.fromISO(finishedAt)
+            .diff(DateTime.fromISO(startedAt))
+            .reconfigure({ locale: i18n.resolvedLanguage.replace("_", "-") })
+            .rescale()
+            .toHuman()}
+        </td>
+      );
     }
     if (startedAt) {
       return <td>{t("joblist.running")}</td>;
