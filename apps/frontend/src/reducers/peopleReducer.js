a352aa10efd878f9e67f0bd70f7f51a4642f2c16
Juan Manuel Canahuate Vargas <29661633+Nerodacles@users.noreply.github.com>
2023-04-30 21:16:57 -0400

    sort persons alphabetically

--- apps/frontend/src/reducers/peopleReducer.js
+++ apps/frontend/src/reducers/peopleReducer.js
@@ -19,6 +19,13 @@ export default function reducer(state = initialState, action) {
       return { ...state, fetching: false, error: action.payload };
     }
     case "FETCH_PEOPLE_FULFILLED": {
+      // sort alphabetically
+      action.payload.sort((a, b) => {
+          if (a.value < b.value) { return -1; }
+          if (a.value > b.value) { return 1; }
+          return 0;
+        });
+
       return {
         ...state,
         fetching: false,
