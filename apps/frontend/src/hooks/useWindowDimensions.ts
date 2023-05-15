52eb679bb6c869e3e971fe6a8c404183ead22776
sickelap <sickelap@pm.me>
2023-05-15 21:23:33 +0100

    refactor things album list to typescript and use rtk

---
+++ apps/frontend/src/hooks/useWindowDimensions.ts
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+
+export function useWindowDimensions() {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const [height, setHeight] = React.useState(window.innerHeight);
+
+  const updateWidthAndHeight = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidthAndHeight);
+    return () => window.removeEventListener("resize", updateWidthAndHeight);
+  });
+
+  return {
+    width,
+    height,
+  };
+}
