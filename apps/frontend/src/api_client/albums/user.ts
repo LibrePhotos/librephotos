a509d4f685277d7fb2ec0f2c48fa52f48b9e9b95
sickelap <sickelap@pm.me>
2023-05-17 22:31:50 +0100

    refactor user albums to use RTK

---
+++ apps/frontend/src/api_client/albums/user.ts
@@ -0,0 +1,106 @@
+import { showNotification } from "@mantine/notifications";
+import { z } from "zod";
+
+import { PhotoSuperSimpleSchema } from "../../actions/albumActions.types";
+import { DatePhotosGroupSchema, SimpleUserSchema } from "../../actions/photosActions.types";
+import i18n from "../../i18n";
+import { api } from "../api";
+
+const UserAlbumListSchema = z
+  .object({
+    id: z.number(),
+    title: z.string(),
+    cover_photo: PhotoSuperSimpleSchema,
+    photo_count: z.number(),
+    owner: SimpleUserSchema,
+    shared_to: SimpleUserSchema.array(),
+    created_on: z.string(),
+    favorited: z.boolean(),
+  })
+  .array();
+const UserAlbumListResponseSchema = z.object({
+  results: UserAlbumListSchema,
+});
+
+export type UserAlbumList = z.infer<typeof UserAlbumListSchema>;
+
+const UserAlbumDetailsSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  owner: SimpleUserSchema,
+  shared_to: SimpleUserSchema.array(),
+
+  date: z.string(),
+  location: z.string().nullable(),
+});
+export type UserAlbumDetails = z.infer<typeof UserAlbumDetailsSchema>;
+
+export const UserAlbumSchema = UserAlbumDetailsSchema.extend({
+  grouped_photos: DatePhotosGroupSchema.array(),
+});
+
+enum Endpoints {
+  fetchUserAlbums = "fetchUserAlbums",
+  fetchUserAlbum = "fetchUserAlbum",
+  createUserAlbum = "createUserAlbum",
+  renameUserAlbum = "renameUserAlbum",
+  deleteUserAlbum = "deleteUserAlbum",
+  removePhotoFromUserAlbum = "removePhotoFromUserAlbum",
+  addPhotoToUserAlbum = "addPhotoToUserAlbum",
+  setUserAlbumCover = "setUserAlbumCover",
+  shareUserAlbum = "shareUserAlbum",
+}
+
+export const userAlbumsApi = api
+  .injectEndpoints({
+    endpoints: builder => ({
+      [Endpoints.fetchUserAlbums]: builder.query<UserAlbumList, void>({
+        query: () => "albums/user/list/",
+        transformResponse: response => UserAlbumListResponseSchema.parse(response).results,
+      }),
+      [Endpoints.deleteUserAlbum]: builder.mutation<void, { id: string; albumTitle: string }>({
+        query: ({ id }) => ({
+          url: `albums/user/${id}/`,
+          method: "DELETE",
+          body: {},
+        }),
+        transformResponse: (response, meta, query) => {
+          showNotification({
+            message: i18n.t<string>("toasts.deletealbum", query),
+            title: i18n.t<string>("toasts.deletealbumtitle"),
+            color: "teal",
+          });
+        },
+      }),
+      [Endpoints.renameUserAlbum]: builder.mutation<void, { id: string; title: string; newTitle: string }>({
+        query: ({ id, title }) => ({
+          url: `albums/user/${id}/`,
+          method: "PATCH",
+          body: { title },
+        }),
+        transformResponse: (response, meta, query) => {
+          showNotification({
+            message: i18n.t<string>("toasts.renamealbum", {
+              oldTitle: query.title,
+              newTitle: query.newTitle,
+            }),
+            title: i18n.t<string>("toasts.renamealbumtitle"),
+            color: "teal",
+          });
+        },
+      }),
+    }),
+  })
+  .enhanceEndpoints<"UserAlbums" | "UserAlbum">({
+    addTagTypes: ["UserAlbums", "UserAlbum"],
+    endpoints: {
+      [Endpoints.fetchUserAlbums]: {
+        providesTags: ["UserAlbums"],
+      },
+      [Endpoints.deleteUserAlbum]: {
+        invalidatesTags: ["UserAlbums"],
+      },
+    },
+  });
+
+export const { useFetchUserAlbumsQuery, useDeleteUserAlbumMutation, useRenameUserAlbumMutation } = userAlbumsApi;
