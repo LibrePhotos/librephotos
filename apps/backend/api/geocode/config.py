d2aaa898b1c679f149965b20becda4009dfd667c
sickelap <sickelap@pm.me>
2023-09-09 15:11:34 +0100

    use method to retrieve config

--- apps/backend/api/geocode/config.py
+++ apps/backend/api/geocode/config.py
@@ -6,45 +6,49 @@ from .parsers.opencage import parse as parse_opencage
 from .parsers.photon import parse as parse_photon
 from .parsers.tomtom import parse as parse_tomtom
 
-config = {
-    "mapbox": {
-        "geocode_args": {"api_key": settings.MAP_API_KEY},
-        "parser": parse_mapbox,
-    },
-    "maptiler": {
-        "geocode_args": {"api_key": settings.MAP_API_KEY},
-        "parser": parse_mapbox,
-    },
-    "tomtom": {
-        "geocode_args": {"api_key": settings.MAP_API_KEY},
-        "parser": parse_tomtom,
-    },
-    "photon": {
-        "geocode_args": {
-            "domain": "photon.komoot.io",
+
+def _get_config():
+    return {
+        "mapbox": {
+            "geocode_args": {"api_key": settings.MAP_API_KEY},
+            "parser": parse_mapbox,
         },
-        "parser": parse_photon,
-    },
-    "nominatim": {
-        "geocode_args": {"user_agent": "librephotos"},
-        "parser": parse_nominatim,
-    },
-    "opencage": {
-        "geocode_args": {
-            "api_key": settings.MAP_API_KEY,
+        "maptiler": {
+            "geocode_args": {"api_key": settings.MAP_API_KEY},
+            "parser": parse_mapbox,
         },
-        "parser": parse_opencage,
-    },
-}
+        "tomtom": {
+            "geocode_args": {"api_key": settings.MAP_API_KEY},
+            "parser": parse_tomtom,
+        },
+        "photon": {
+            "geocode_args": {
+                "domain": "photon.komoot.io",
+            },
+            "parser": parse_photon,
+        },
+        "nominatim": {
+            "geocode_args": {"user_agent": "librephotos"},
+            "parser": parse_nominatim,
+        },
+        "opencage": {
+            "geocode_args": {
+                "api_key": settings.MAP_API_KEY,
+            },
+            "parser": parse_opencage,
+        },
+    }
 
 
 def get_provider_config(provider) -> dict:
+    config = _get_config()
     if provider not in config:
         raise Exception(f"Map provider not found: {provider}.")
     return config[provider]["geocode_args"]
 
 
 def get_provider_parser(provider) -> callable:
+    config = _get_config()
     if provider not in config:
         raise Exception(f"Map provider not found: {provider}.")
     return config[provider]["parser"]
