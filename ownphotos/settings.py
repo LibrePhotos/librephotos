ee670ef95dacdbf24507a3eeb03c4dd1296e5124
Niaz <faridani.rad@gmail.com>
2023-09-14 13:27:15 +0200

    Fix #858

--- ownphotos/settings.py
+++ ownphotos/settings.py
@@ -25,6 +25,7 @@ for envvar in (
     if envvar not in os.environ:
         raise NameError("Environment variable not set :" + envvar)
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
