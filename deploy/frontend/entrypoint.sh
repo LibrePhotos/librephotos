40e6b13f241703464167abadce256b22f45c71d4
Niaz <faridani.rad@gmail.com>
2023-09-25 16:29:12 +0200

    Do not clear console when running
    This will help debugging on startup

--- deploy/frontend/entrypoint.sh
+++ deploy/frontend/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 echo "installing frontend"
-npm install --legacy-peer-deps
-npm run postinstall
-npm run start
-
+FORCE_COLOR=true npm install --legacy-peer-deps | cat
+FORCE_COLOR=true npm postinstall | cat
+FORCE_COLOR=true npm start | cat
 # DANGEROUSLY_DISABLE_HOST_CHECK=true HOST=0.0.0.0 npm start
