3a878df8e68cbd5c25d13a65f667a49aafd8ca28
Niaz <faridani.rad@gmail.com>
2023-12-30 16:14:48 +0100

    Add llm setting to response

--- apps/backend/api/schemas/site_settings.py
+++ apps/backend/api/schemas/site_settings.py
@@ -8,6 +8,7 @@ site_settings_schema = {
         {"required": ["map_api_provider"]},
         {"required": ["map_api_key"]},
         {"required": ["captioning_model"]},
+        {"required": ["llm_model"]},
     ],
     "properties": {
         "allow_registration": {"type": "boolean"},
@@ -17,5 +18,6 @@ site_settings_schema = {
         "map_api_provider": {"type": "string"},
         "map_api_key": {"type": "string"},
         "captioning_model": {"type": "string"},
+        "llm_model": {"type": "string"},
     },
 }
