5b87f9b2d411b3ec7ba382da96860a225ca61d60
Niaz <faridani.rad@gmail.com>
2024-04-25 20:35:39 +0200

    Start services with Django

--- deploy/backend-gpu/entrypoint.sh
+++ deploy/backend-gpu/entrypoint.sh
@@ -15,12 +15,7 @@ python manage.py showmigrations | tee /logs/show_migrate.log
 python manage.py migrate | tee /logs/command_migrate.log
 python manage.py showmigrations | tee /logs/show_migrate.log
 python manage.py collectstatic --no-input
-python image_similarity/main.py 2>&1 | tee /logs/image_similarity.log &
-python service/thumbnail/main.py 2>&1 | tee /logs/thumbnail.log &
-python service/face_recognition/main.py 2>&1 | tee /logs/face_recognition.log &
-python service/clip_embeddings/main.py 2>&1 | tee /logs/clip_embeddings.log &
-python service/image_captioning/main.py 2>&1 | tee /logs/image_captioning.log &
-python service/llm/main.py 2>&1 | tee /logs/llm.log &
+python manage.py start_service all
 python manage.py clear_cache 
 python manage.py build_similarity_index 2>&1 | tee /logs/command_build_similarity_index.log
 
