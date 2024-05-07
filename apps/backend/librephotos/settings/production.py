49ec07b6288fad3f48c289e1ff7cf8c47fa8d003
Niaz Faridani-Rad <faridani.rad@gmail.com>
2024-05-07 10:44:35 +0200

    We use JWT for auth

--- apps/backend/librephotos/settings/production.py
+++ apps/backend/librephotos/settings/production.py
@@ -180,7 +180,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
