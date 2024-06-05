decacd500faf0511ab47b312c3ba64bc519f8f42
Niaz <faridani.rad@gmail.com>
2024-06-05 10:48:01 +0200

    Directly access processes and do not save them in a list

--- apps/backend/api/services.py
+++ apps/backend/api/services.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 import requests
 
@@ -12,17 +13,16 @@ SERVICES = {
     "clip_embeddings": 8006,
     "llm": 8008,
     "image_captioning": 8007,
+    "exif": 8010,
+    "tags": 8011,
 }
-
-running_processes = {}
 
 
 def check_services():
-    for service in running_processes.keys():
+    for service in SERVICES.keys():
         if not is_healthy(service):
-            process = running_processes.pop(service)
-            process.terminate()
-            logger.info(f"Starting {service}")
+            stop_service(service)
+            logger.info(f"Restarting {service}")
             start_service(service)
 
 
@@ -30,15 +30,20 @@ def is_healthy(service):
     port = SERVICES.get(service)
     try:
         res = requests.get(f"http://localhost:{port}/health")
+        # If response has timestamp, check if it needs to be restarted
+        if res.json().get("last_request_time") is not None:
+            if res.json()["last_request_time"] < time.time() - 120:
+                logger.info(f"Service {service} is stale and needs to be restarted")
+                return False
         return res.status_code == 200
     except BaseException as e:
-        logger.warning(f"Error checking health of {service}: {str(e)}")
+        logger.exception(f"Error checking health of {service}: {str(e)}")
         return False
 
 
 def start_service(service):
     if service == "image_similarity":
-        process = subprocess.Popen(
+        subprocess.Popen(
             [
                 "python",
                 "image_similarity/main.py",
@@ -46,7 +51,7 @@ def start_service(service):
             ]
         )
     elif service in SERVICES.keys():
-        process = subprocess.Popen(
+        subprocess.Popen(
             [
                 "python",
                 f"service/{service}/main.py",
@@ -57,17 +62,37 @@ def start_service(service):
         logger.warning("Unknown service:", service)
         return False
 
-    running_processes[service] = process
     logger.info(f"Service '{service}' started successfully")
     return True
 
 
 def stop_service(service):
-    if service in running_processes:
-        process = running_processes.pop(service)
-        process.terminate()
-        logger.info(f"Service '{service}' stopped successfully")
+    try:
+        # Find the process ID (PID) of the service using `ps` and `grep`
+        ps_command = f"ps aux | grep '[p]ython.*{service}/main.py' | awk '{{print $2}}'"
+        result = subprocess.run(
+            ps_command,
+            shell=True,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        pids = result.stdout.decode().strip().split()
+
+        if not pids:
+            logger.warning("Service '%s' is not running", service)
+            return False
+
+        # Kill each process found
+        for pid in pids:
+            subprocess.run(["kill", "-9", pid], check=True)
+            logger.info(f"Service '{service}' with PID {pid} stopped successfully")
+
         return True
-    else:
-        logger.warning(f"Service '{service}' is not running")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Failed to stop service '{service}': {e.stderr.decode().strip()}")
+        return False
+    except Exception as e:
+        logger.error(f"An error occurred while stopping service '{service}': {e}")
         return False
