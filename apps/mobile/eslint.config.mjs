77dcfff9965798f18dea9b7fa1a2b5e8792fb5d2
Niaz <faridani.rad@gmail.com>
2024-07-14 11:17:08 +0200

    Fix more bugs

--- apps/mobile/eslint.config.mjs
+++ apps/mobile/eslint.config.mjs
@@ -1,44 +1,50 @@
-import jest from "eslint-plugin-jest";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import jest from 'eslint-plugin-jest'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 
-export default [...compat.extends("@react-native-community"), {
+export default [
+  ...compat.extends('@react-native'),
+  {
     plugins: {
-        jest,
+      jest,
     },
 
     languageOptions: {
-        globals: {
-            ...jest.environments.globals.globals,
-        },
+      globals: {
+        ...jest.environments.globals.globals,
+      },
     },
 
     settings: {
-        "import/resolver": {
-            "babel-module": {},
-        },
+      'import/resolver': {
+        'babel-module': {},
+      },
     },
 
     rules: {
-        semi: ["error", "never"],
-        "object-curly-spacing": ["error", "always"],
-        "array-bracket-spacing": ["error", "never"],
-        "react/require-default-props": ["error"],
-        "react/default-props-match-prop-types": ["error"],
-        "react/sort-prop-types": ["error"],
+      semi: ['error', 'never'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'react/require-default-props': ['error'],
+      'react/default-props-match-prop-types': ['error'],
+      'react/sort-prop-types': ['error'],
 
-        "prettier/prettier": ["error", {
-            endOfLine: "auto",
-        }],
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
     },
-}];
+  },
+]
