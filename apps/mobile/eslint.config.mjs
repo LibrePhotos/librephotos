0c50c6d3e5e15aca9bf57a73760157c2c7be6144
Niaz <faridani.rad@gmail.com>
2024-07-14 10:53:20 +0200

    Update elint config

---
+++ apps/mobile/eslint.config.mjs
@@ -0,0 +1,44 @@
+import jest from "eslint-plugin-jest";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("@react-native-community"), {
+    plugins: {
+        jest,
+    },
+
+    languageOptions: {
+        globals: {
+            ...jest.environments.globals.globals,
+        },
+    },
+
+    settings: {
+        "import/resolver": {
+            "babel-module": {},
+        },
+    },
+
+    rules: {
+        semi: ["error", "never"],
+        "object-curly-spacing": ["error", "always"],
+        "array-bracket-spacing": ["error", "never"],
+        "react/require-default-props": ["error"],
+        "react/default-props-match-prop-types": ["error"],
+        "react/sort-prop-types": ["error"],
+
+        "prettier/prettier": ["error", {
+            endOfLine: "auto",
+        }],
+    },
+}];
