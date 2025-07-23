d0d7efcb9c115786054273a4c7aa399a9d2e4c5c
Niaz <faridani.rad@gmail.com>
2025-07-23 21:23:29 +0200

    chore(deps): remove unused dependencies from package.json and yarn.lock

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,22 +1,20 @@
 import "./wdyr";
 
 import "@mantine/core/styles.css";
-import "font-awesome/css/font-awesome.min.css";
+import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 // css
 import { createRoot } from "react-dom/client";
 import "react-leaflet-markercluster/dist/styles.min.css";
 import "react-vis/dist/style.css";
 
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './api_client/api';
-
 import { App } from "./App";
+import { queryClient } from "./api_client/api";
 
 const container = document.getElementById("root");
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
