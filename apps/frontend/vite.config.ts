43c4b0e9078fe04621a30b0503f39edded1859d0
Niaz <faridani.rad@gmail.com>
2025-07-25 07:54:05 +0200

    chore(deps): update dependencies in package.json and yarn.lock

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,11 +1,10 @@
-import react from "@vitejs/plugin-react-swc";
-import viteTsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   base: "/",
-  plugins: [react(), viteTsconfigPaths(), TanStackRouterVite({target: "react", autoCodeSplitting: true})],
+  plugins: [tanstackRouter({target: "react", autoCodeSplitting: true}), react()],
   server: {
     host: "0.0.0.0",
     port: 3000,
