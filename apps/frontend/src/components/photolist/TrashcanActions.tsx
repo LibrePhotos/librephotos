974741152d69b96754d9b1b37729f6e28711f8db
Niaz <faridani.rad@gmail.com>
2025-07-28 13:20:42 +0200

    Improve deletion dialog

--- apps/frontend/src/components/photolist/TrashcanActions.tsx
+++ apps/frontend/src/components/photolist/TrashcanActions.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Button, Group, Modal } from "@mantine/core";
+import { ActionIcon, Button, Group, Modal, Stack, Text, Title, Space, Tooltip } from "@mantine/core";
 import { IconArrowBackUp as ArrowBackUp, IconTrash as Trash } from "@tabler/icons-react";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
@@ -19,59 +19,108 @@ export function TrashcanActions(props: Readonly<Props>) {
   const markPhotosDeleted = useMarkPhotosDeletedMutation();
   const purgeDeletedPhotos = usePurgeDeletedPhotosMutation();
 
+  const closeDialog = () => {
+    setOpenDeleteDialog(false);
+  };
+
+  const handlePermanentDelete = () => {
+    purgeDeletedPhotos.mutate({ image_hashes: selectedItems.map(i => i.id) });
+    updateSelectionState({
+      selectMode: false,
+      selectedItems: [],
+    });
+    closeDialog();
+  };
+
+  const selectedCount = selectedItems.length;
+
   return (
     <Group>
       {location.pathname.startsWith("/deleted") && (
         <>
-          <ActionIcon
-            disabled={selectedItems.length === 0}
-            onClick={() => {
-              markPhotosDeleted.mutate({
-                image_hashes: selectedItems.map(i => i.id),
-                deleted: false,
-              });
-              updateSelectionState({
-                selectMode: false,
-                selectedItems: [],
-              });
-            }}
+          <Tooltip 
+            label={selectedCount === 1 ? "Restore photo" : `Restore ${selectedCount} photos`} 
+            position="bottom" 
+            withArrow
           >
-            <ArrowBackUp />
-          </ActionIcon>
-          <ActionIcon
-            disabled={selectedItems.length === 0}
-            onClick={() => {
-              setOpenDeleteDialog(true);
-            }}
+            <ActionIcon
+              disabled={selectedCount === 0}
+              variant="light"
+              color="blue"
+              size="lg"
+              onClick={() => {
+                markPhotosDeleted.mutate({
+                  image_hashes: selectedItems.map(i => i.id),
+                  deleted: false,
+                });
+                updateSelectionState({
+                  selectMode: false,
+                  selectedItems: [],
+                });
+              }}
+            >
+              <ArrowBackUp />
+            </ActionIcon>
+          </Tooltip>
+          
+          <Tooltip 
+            label={selectedCount === 1 ? "Delete permanently" : `Delete ${selectedCount} photos permanently`} 
+            position="bottom" 
+            withArrow
           >
-            <Trash color="red" />
-          </ActionIcon>
+            <ActionIcon
+              disabled={selectedCount === 0}
+              variant="light"
+              color="red"
+              size="lg"
+              onClick={() => {
+                setOpenDeleteDialog(true);
+              }}
+            >
+              <Trash />
+            </ActionIcon>
+          </Tooltip>
         </>
       )}
-      <Modal opened={openDeleteDialog} onClose={() => setOpenDeleteDialog(false)} title="Delete images">
-        <Group justify="center">
-          <Button
-            color="blue"
-            onClick={() => {
-              setOpenDeleteDialog(false);
-            }}
-          >
-            {t("cancel")}
-          </Button>
-          <Button
-            color="red"
-            onClick={() => {
-              purgeDeletedPhotos.mutate({ image_hashes: selectedItems.map(i => i.id) });
-              updateSelectionState({
-                selectMode: false,
-                selectedItems: [],
-              });
-              setOpenDeleteDialog(false);
-            }}
-          >
-            {t("confirm")}
-          </Button>
-        </Group>
+      
+      <Modal 
+        opened={openDeleteDialog} 
+        onClose={closeDialog} 
+        centered
+        size="md"
+        title={
+          <Title order={5}>
+            <span style={{ paddingRight: "8px" }}>
+              <Trash size={20} />
+            </span>
+            {t("toasts.finaldeletephototitle")}
+          </Title>
+        }
+      >
+        <Stack>
+          <Text size="sm">
+            You are about to permanently delete {selectedCount} {selectedCount === 1 ? 'image' : 'images'}. 
+            {selectedCount === 1 ? 'This image' : 'These images'} will be completely removed from your library and storage.
+          </Text>
+          
+          <Text size="sm" c="red" fw={500}>
+            {t("adminarea.cannotbeundone")}
+          </Text>
+          
+          <div style={{ display: "flex", justifyContent: "flex-end", marginTop: "16px" }}>
+            <Button variant="default" onClick={closeDialog}>
+              {t("cancel")}
+            </Button>
+            <Space w="md" />
+            <Button 
+              color="red" 
+              onClick={handlePermanentDelete}
+              leftSection={<Trash size={16} />}
+            >
+              {t("delete")}
+            </Button>
+          </div>
+        </Stack>
       </Modal>
     </Group>
   );
